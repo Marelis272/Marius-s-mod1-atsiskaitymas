e3379da705b0074a5b5eee4c6987ba6e8b0f2ad9
Marelis272 <Mariusstosk@gmail.com>
2024-11-20 19:34:08 +0200

    Created crawling function to get article titles from given URL in a set time period

---
+++ marius_s_mod1_atsiskaitymas/main.py
@@ -0,0 +1,24 @@
+import requests
+from bs4 import BeautifulSoup
+import time
+
+base_url_1 = "https://www.lrytas.lt/"
+
+def crawling(url, timeout):
+    start_time = time.time()
+    title_list = []
+
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+
+        soup = BeautifulSoup(response.content, "html.parser")
+
+        article_titles = soup.find_all('h2', class_='text-base font-medium text-black-custom')
+
+        for title in article_titles:
+            if time.time() - start_time > timeout:
+                print("Timeout reached - stopping the function")
+                return
+            link = title.find('a')
+            title_list.append(link.text.strip)
